77880c214c07c7216221bdc57665779a5c50493f
Daniel Jorgensen <djorg386@gmail.com>
2019-11-10 15:59:54 -0500

    auto-generate typescript declaration files

--- index.d.ts
+++
@@ -1,7 +0,0 @@
-import SweetAlert from './src/components/SweetAlert';
-
-declare module 'react-bootstrap-sweetalert' {}
-
-export { SweetAlertProps, SweetAlertType } from './src/components/SweetAlert';
-
-export default SweetAlert;
